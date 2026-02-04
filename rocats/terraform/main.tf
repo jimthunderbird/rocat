6382596161ed7b781124349846f69407fbbdf54d
Jim Huang <jim.javathunderbird@gmail.com>
2026-02-03 23:02:39 -0800

    use append

--- rocats/terraform/main.tf
+++ rocats/terraform/main.tf
@@ -1,3 +1,14 @@
+variable "instance_name" {
+  description = "Value of the Name tag for the EC2 instance"
+  type        = string
+  default     = "Pro-Trading-Server"
+}
+
+variable "ami_id" {
+  type    = string
+  default = "ami-0c55b159cbfafe1f0"
+}
+
 provider "aws" {
   region = "us-west-2"
 }
@@ -8,5 +19,14 @@ resource "aws_instance" "example_server" {
 
   tags = {
     Name = "TerraformDemoInstance"
+  }
+}
+
+resource "aws_instance" "app_server" {
+  ami           = var.ami_id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = var.instance_name
   }
 }
