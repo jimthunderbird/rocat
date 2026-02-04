6ea71cc5c2a12d8bdd1af8a456742366712bebaf
Jim Huang <jim.javathunderbird@gmail.com>
2026-02-04 13:04:05 -0800

    lazy loading and scoping

--- rocats/terraform/main.tf
+++ rocats/terraform/main.tf
@@ -39,3 +39,23 @@ resource "aws_instance" "app_server" {
     Name = var.instance_name
   }
 }
+
+variable "instance_name_2" {
+  description = "Value of the Name tag for the EC2 instance"
+  type        = string
+  default     = "Pro-Trading-Server_2"
+}
+
+variable "ami_id_2" {
+  type    = string
+  default = "ami-0c55b159123456"
+}
+
+resource "aws_instance" "app_server_2" {
+  ami           = var.ami_id_2
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = var.instance_name_2
+  }
+}
