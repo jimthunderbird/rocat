fe0492ea5fcf7e12f9f645a93e598c25d6af1818
Jim Huang <jim.javathunderbird@gmail.com>
2026-02-03 22:50:31 -0800

    add terraform rocat

---
+++ rocats/terraform/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_instance" "example_server" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "TerraformDemoInstance"
+  }
+}
